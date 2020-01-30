f0cb72ba04e2bea2584dcd4f7be09ea298cb624e
shuvidora <2245862630@qq.com>
2020-01-31 02:01:12 +0800

    Update Sqrt.go
    update

--- go/Sqrt.go
+++ go/Sqrt.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "math"
-    "fmt"
+	"fmt"
+	"math"
 )
 
 // Sqrt returns the square root of x.
 func Sqrt(x float64) (z float64) {
 
-    if x <= 0 || x == 1 || math.IsNaN(x) || math.IsInf(x, 1) {
-        return x
-    }
+	if x <= 0 || x == 1 || math.IsNaN(x) || math.IsInf(x, 1) {
+		return x
+	}
 
-    z = x / 2
-    
+	z = x / 2
+
 	for i := 0; i <= 35; i++ {
 		// 2z is the derivative of z²
-		cacl := z - (z*z - x) / (2*z)
+		cacl := z - (z*z-x)/(2*z)
 
 		if cacl == z {
-            break;
+			break
 		}
 
 		z = cacl
@@ -29,6 +29,6 @@ func Sqrt(x float64) (z float64) {
 }
 
 func main() {
-    fmt.Println(Sqrt(1<<64 - 1))
-    fmt.Println(math.Sqrt(1<<64 -1))
+	fmt.Println(Sqrt(1<<64 - 1))
+	fmt.Println(math.Sqrt(1<<64 - 1))
 }
