ff1c38771c086067151a9b8e83235de77187a7b5
shuvidora <2245862630@qq.com>
2020-01-31 01:43:53 +0800

    Create Sqrt.go

---
+++ go/Sqrt.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "math"
+    "fmt"
+)
+
+// Sqrt returns the square root of x.
+func Sqrt(x float64) (z float64) {
+
+    if x <= 0 || math.IsNaN(x) || math.IsInf(x, 1) {
+        return x
+    }
+
+    z = x / 2
+    
+	for i := 0; i <= 35; i++ {
+		// 2z is the derivative of z²
+		cacl := z - (z*z - x) / (2*z)
+
+		if cacl == z {
+            break;
+		}
+
+		z = cacl
+	}
+
+	return
+}
+
+func main() {
+    fmt.Println(Sqrt(1<<64 - 1))
+    fmt.Println(math.Sqrt(1<<64 -1))
+}
